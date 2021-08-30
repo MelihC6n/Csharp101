5a4e268569233f41d575773aae9c10b953ebda36
Melih Can Akgunes <melihcanakgunes@gmail.com>
2021-08-30 17:17:59 +0300

    first

---
+++ Console-Programlama/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Console_Programlama
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("İsminiz nedir?");
+            string name=Console.ReadLine();
+            Console.WriteLine("Soyisminiz nedir?");
+            string surname=" "+Console.ReadLine();
+            Console.WriteLine("Merhaba " + name + surname);
+        }
+    }
+}
